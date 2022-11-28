2075496035516f74d094e99325e4499dbca4895b
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-28 09:35:14 +0530

    forms modue added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewfriendsComponent } from './viewfriends/viewfriends.component';
 import { SearchfriendsComponent } from './searchfriends/searchfriends.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const myRoutes:Routes=[
   {
@@ -35,7 +36,8 @@ const myRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoutes)
+    RouterModule.forRoot(myRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
